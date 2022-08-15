6e73b5914d1240ce0cae421b6586869f73a28589
dnlclmnt <dnlclmnt@gmail.com>
2022-08-14 21:11:49 -0700

    completed assessment questions

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,11 @@
 // --------------------1) What will this log?
 
 const cohort = "Echo 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The split method called on the cohort string will result in an array of characters from that string. It will output like:
+// ['E', 'c', 'h', 'o', ' ', '2', '0', '2', '2']
+// b) Verify and explain: In the MDN documentation and from my own personal experience using the split method several times to solve leetcode problems, I know that the split method returns a new array if called on a string. because "" was used as the separator, the string will be split on every character.
 
 
 // --------------------2) What will this log?
@@ -19,28 +20,28 @@ const cohort = "Echo 2022"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: There is no return keyword inside of the greeter function. Due to there being no return value, greeter will return undefined. 
+// b) Verify and explain: Though the function's logic is sound, if we don't give a value to be returned, logging it into the console shows nothing, as there is no actual value to be returned. 
 
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map(number => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: The map method is being called on the array of [4,5,6,7,8]. This will return a new array with whatever argument we want to pass into the method. The word 'number' is being used as the keyword for each value in the array (remembering the parameter syntax for higher order functions is generally (element, index, array)). The new array being returned will be each number in the passed in array multiplied by two. 
 
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter(number => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: Remembering our parameter syntax for higher order functions is (element, index, array), we can see that 'number' is the keyword being used to represent each element in the new array that will be returned. The filter method returns to us a new array that meets the criteria of the argument being passed in (giving a 'true' value). Because 11, 13, and 15 would return true to being odd numbers, they are the only values returned inside of the new array. The 'even' values are filtered out.
 
 
 // --------------------5) What will this log?
@@ -51,10 +52,20 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "JavaScript" will be returned.
+// b) Verify and explain: Objects can be accessed using dot notation, so the languages property on the myCodingSkills object is the first item being accessed. Due to the languages property containing an array within it, we can access the first value within the array by calling the array on [0]. This returns "JavaScript". To make this easier to read and create a step-by-step process, we could create our own set of variables as such:
+
+
+// let codingLanguages = myCodingSkills.languages;  // this gives us our array of languages, accessing the languages property on myCodingSkills
+// let jsLanguage = codingLanguages[0];  // this accesses the first item in the array
+
+// console.log(jsLanguage)
+
+
+// Breaking it down like this can make accessing properties much easier to understand and decipher.
+
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +78,14 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: learnStudent is creating a new instance of the Learn class. We are giving "George" as the argument for "name" in the constructor method and that will be used as the value for the student property. The console will log our new object with the student property being "George". All other properties are already declared as "Echo" and 2022 respectively. It will look like :
+
+// Learn {
+  // student: "George",
+  // cohort: "Echo",
+  // year: 2022
+// }
+
+// b) Verify and explain: We are logging the learnStudent object that was created with the Learn class. Classes allow us to create templates or "blueprints" for new objects to be created with.
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: Arguments are passed into a function invocation and are the actual values used for the parameters. Parameters are placeholders whose values will be defined by the arguments passed into the function. 
 
-Researched answer:
+Researched answer: Without an argument, parameters typically are undefined.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: The predefined parameters are (element, index, array). The element parameter is required, and the two others are completely optional.
 
-Researched answer:
+Researched answer: The element parameter can also be seen as the current value. 
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: The map method creates a new array with elements from the original array modified in whatever way we define. The filter method filters out values from an existing array and returns to us a new array of values that met the filter criteria.
 
-Researched answer:
+Researched answer: The map method will take an array, loop over the array, and return a new array with whatever argument we pass into it. This method is useful for modifying data within an array. For example, we can have an existing array of numbers and use the map method to return another array with the numbers of the first array multiplied by 2. It is important to note that the map method will return a new array with the same length of the original arrray. The filter method returns a new array with only values that meet the criteria of the conditional statement defined within it(aka return a 'true' value). The values that are 'false' get filtered out of the array, while the ones that are true are pushed into a new array. The returning result will be that new array.
 
 4. What is the DOM?
 
-Your answer:
+Your answer: The DOM allows us to use JavaScript and modify or access elements within HTML pages. 
 
-Researched answer:
+Researched answer: DOM stands for Document Object Model. It is a web API and can be seen as the connecting point between JavaScript and HTML documents. It is how we are able to use code to affect or manipulate web pages.
 
 5. What is React? Why would you use it?
 
-Your answer:
+Your answer: React is a JavaScript library that uses components to create web pages. You would use it to quickly create dynamic web page UIs and because of its large open-source support and community.
 
-Researched answer:
+Researched answer: It was created by Facebook and allows us to reuse blocks of code (components) multiple times. React makes creating user interfaces much easier by separating the blocks of code into smaller components.
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: I have limited knowledge on hoisting, but I know it involves certain variables and functions being moved to the top of their scope.
 
-Researched answer:
+Researched answer: Hoisting is a process in JavaScript that moves variable and function declarations to the top of their scope, allowing them to be called before they're declared.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. User stories:
+1. User stories: It is a quick and simplified description of what features in a system or piece of software should work and/or how those features will work.
 
-2. Spread operator:
+2. Spread operator: The spread operator unpacks all array elements inside of an array and can be used within another array or object with (...). This can be used instead of the concat method to join two arrays together and can also be used with destructuring.
 
-3. React props:
+3. React props: Props (or properties) are used to pass data into components. They are similar to function arguments in JavaScript.
 
-4. Conditional rendering:
+4. Conditional rendering: Displays components based on conditional criteria being met.
 
-5. DOM events:
+5. DOM events: The interaction between user and the HTML page(clicking on objects or words, mouse hovers, etc)
